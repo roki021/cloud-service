dab26bfa586ae8aea91aefcbbe788c8244177d3c
Mikec <mikecitt@gmail.com>
2020-01-23 20:45:10 +0100

    Sitne ispravke.

--- static/js/users.js
+++ static/js/users.js
@@ -4,35 +4,60 @@ function getUsers() {
         type: "GET",
         dataType: "json",
         complete: function(data) {
+            $("#canvas").empty();
             if(data.status == 403) {
                 $("#canvas").append('<h1>403 Forbidden</h1>');
             } else {
                 response = data.responseJSON;
                 var table = $("<table/>");
-                table.append("<thead><tr><th>1</th><th>2</th></tr></thead>")
+                table.append("<thead><tr><th>Email</th><th>First Name</th><th>Last Name</th><th>Organization</th></tr></thead>")
                 table.addClass("table table-hover table-dark");
+                var tbody = $("<tbody/>");
+                table.append(tbody);
 
-
-
-
-                table.append("<td>test</td>")
-                $("#canvas").append(table);
-                /*var header =
-                `
-
-                `
-                for(let user in response) {
+                for(let user of response) {
+                    if(user.organization == undefined)
+                        user.organization = "-";
                     var row =
                     `
                         <tr>
-                            <td></td>
+                            <td>${user.email}</td>
+                            <td>${user.firstName}</td>
+                            <td>${user.lastName}</td>
+                            <td>${user.organization}</td>
                         </tr>
                     `
-                }*/
+                    tbody.append(row);
+                }
+                $("#canvas").append(table);
             }
-
-            console.log(data)
-            console.log(response);
         }
     });
+}
+
+function removeUser(email) {
+    $.ajax({
+        url: "rest/removeUser?email=" + email,
+        type: "GET",
+        dataType: "json",
+        complete: function(data) {
+
+        }
+    });
+}
+
+function addUser() {
+    var data = getFormData($("#addUserForm"));
+    var s = JSON.stringify(data);
+
+    $.ajax({
+        url: "rest/addUser",
+        type: "POST",
+        data: s,
+        contentType: "application/json",
+        dataType: "json",
+        complete: function(data) {
+
+        }
+    })
 }
