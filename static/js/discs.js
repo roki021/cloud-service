b6894323603ed0b990c7877c0d3323611309fc23
roki021 <26367195+roki021@users.noreply.github.com>
2020-01-29 22:16:43 +0100

    Sitne ispravke

--- static/js/discs.js
+++ static/js/discs.js
@@ -228,42 +228,6 @@ function checkFieldsFilterDisc(row) {
     return nameInd && minInd && maxInd;
 }
 
-
-
-(function($) {
-	$.fn.filterTable = function(filter, columnname) {
-		var index = null;
-		this.find("thead > tr:first > th").each(function(i) {
-			if ($.trim($(this).text()) == columnname) {
-				index = i;
-				return false;
-			}
-		});
-		if (index == null)
-			throw ("filter columnname: " + columnname + " not found");
-
-		this.find("tbody:first > tr").each(function() {
-		    var row = $(this);
-		    if (filter == "") {
-		        row.show();
-		    }
-		    else {
-		        var cellText = row.find("td:eq(" + index + ")").find('option:selected').text();
-		        if (cellText == "") {
-		            cellText = $(row.find(("td:eq(" + index + ")"))).text();
-		        }
-		        if (cellText.indexOf(filter) == -1) {
-		            row.hide();
-		        }
-		        else {
-		            row.show();
-		        }
-		    }
-		});
-		return this;
-	};
-})(jQuery);
-
 function setUpAddFormDisc(buttonText, func, route) {
     var canvas = $("#canvas");
     canvas.empty();
